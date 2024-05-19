ee9f9d769d86fc7b8a9d4153cecab0743b77083e
IPGAP\marvin.yaguel <marvin.yaguel@mrm.com>
2024-05-20 03:35:51 +0800

    Code Update: -Endpoint returning all pizza types -Endpoint for sales report of each pizza type

--- Controllers/PizzaPlaceController.cs
+++ Controllers/PizzaPlaceController.cs
@@ -1,5 +1,6 @@
 ﻿using _24h_Code_Challenge.Data;
 using _24h_Code_Challenge.Entities;
+using _24h_Code_Challenge.Model;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -21,13 +22,25 @@ namespace _24h_Code_Challenge.Controllers
 
 
         /// <summary>
-        /// Return list of current pizza products.
+        /// Return list of current pizza products and details.
         /// </summary>
         /// <returns></returns>
         [HttpGet]
-        [Route("products")]
-        public async Task<ActionResult<List<PizzaType>>> Products()
+        [Route("products/{type_id?}")]
+        public async Task<ActionResult<List<PizzaType>>> Products(string type_id = "default")
         {
+            try
+            {
+                if (!type_id.Equals("default"))
+                {
+                    return Ok(await _dataContext.PizzaTypes.Include(p => p.Pizzas).Where(t => t.PizzaTypeId.Equals(type_id)).ToListAsync());
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
             return Ok(await _dataContext.PizzaTypes.Include(p => p.Pizzas).ToListAsync());
         }
 
@@ -40,9 +53,56 @@ namespace _24h_Code_Challenge.Controllers
         [Route("orders/{order_id}")]
         public async Task<ActionResult> Orders(string order_id)
         {
-
             return Ok(await _dataContext.Sales.Where(i => i.OrderId.ToString().Equals(order_id)).ToListAsync());
         }
 
+        /// <summary>
+        /// Return list of all pizza_type_id.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("pizzatypes")]
+        public async Task<ActionResult<List<PizzaVariance>>> PizzaTypes()
+        {
+            List<PizzaVariance> pizzaTypes = [];
+
+            try
+            {
+                var pizza = await _dataContext.PizzaTypes.Select(p => new
+                {
+                    p.Name,
+                    p.PizzaTypeId
+                }).OrderBy(p => p.Name).ToListAsync();
+
+                foreach (var item in pizza)
+                {
+                    pizzaTypes.Add(new PizzaVariance { Name = item.Name, TypeId = item.PizzaTypeId });
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return Ok(pizzaTypes);
+        }
+
+        /// <summary>
+        /// return high to low selling pizzas.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("bestselling")]
+        public async Task<ActionResult> BestSelling()
+        {            
+            var res = await _dataContext.Sales.GroupBy(t => t.PizzaTypeId).Select(x => new
+            {
+                PizzaType = x.Key,
+                TotalSales = x.Sum(y => y.Price)
+            }).OrderByDescending(o => o.TotalSales).ToListAsync();
+           
+            return Ok(res);
+        }
     }
 }
---
+++ Model/PizzaVariance.cs
@@ -0,0 +1,8 @@
+﻿namespace _24h_Code_Challenge.Model
+{
+    public class PizzaVariance
+    {
+        public string Name { get; set; }
+        public string TypeId { get; set; }
+    }
+}
